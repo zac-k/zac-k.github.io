b2e334e87904209316a1fc110020d9148e5bd280
Zac <zac.kemp@outlook.com>
2019-05-15 06:14:20 +1000

    fixed clicking on tracks via music page, but loading no longer working

--- scripts/utils.js
+++ scripts/utils.js
@@ -37,3 +37,21 @@
 		
 		parent.postMessage({label: 'overlay', value:false}, '*');
 	}
+	
+	function generateTrack(track_num) {
+		
+		var div = document.createElement("div");
+		div.class = "tooltip";
+		div.onmouseover = "positionMarker(this); enableOverlay(this)";
+		div.onmouseout = "disableOverlay(this);";
+		
+		var iframe = document.createElement("iframe");
+		iframe.scrolling = "no";
+		iframe.id = "hearthis_at_track_" + track_num.toString();
+		iframe.width = "100%";
+		iframe.height = "130";
+		iframe.src = "https://hearthis.at/embed/" + track_num.toString() + "/transparent/?hcolor=005c61&color=00b0bb&style=2&block_size=5&block_space=2&background=0&waveform=0&cover=1&autoplay=0&css=";
+		
+		
+		
+	}
