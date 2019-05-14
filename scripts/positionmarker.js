109e5090585bbcc0361d1d6bc6a2dc3ebe637860
Zac <zac.kemp@outlook.com>
2019-05-15 05:44:45 +1000

    added loading, temp disabled overlay, not fully functional

--- scripts/positionmarker.js
+++ scripts/positionmarker.js
@@ -10,15 +10,23 @@
 			
 		$(item).find('.tooltiptext #before').css('left', $(item).position().left + centre_offset);
 		$(item).find('.tooltiptext #after').css('left', $(item).position().left + centre_offset);
+	}
+	
+	function enableOverlay(item) {
+		return;
+		
 		$(item).css('z-index', 5)
 		$(item).css('z-index', 5)
-		//// $(item).children().each(function(){$(this).css('z-index', 5);})
+		//$(item).children().each(function(){$(this).css('z-index', 5);})
 	    $(document).find('.overlay').css('opacity', 0.6);
 		
 		parent.postMessage({label: 'overlay', value:true}, '*');
 	}
 	
-	function resetOverlay(item) {
+	
+	
+	function disableOverlay(item) {
+		return;
 		$(item).css('z-index', 0)
 		$(document).find('.overlay').css('opacity', 0);
 		
