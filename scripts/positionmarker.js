4d1380d4bee33f8d7975cef1eb4b43e22b5ecccc
Zac <zac.kemp@outlook.com>
2019-05-14 15:12:43 +1000

    implemented loading for first page

--- scripts/positionmarker.js
+++ scripts/positionmarker.js
@@ -10,5 +10,22 @@
 			
 		$(item).find('.tooltiptext #before').css('left', $(item).position().left + centre_offset);
 		$(item).find('.tooltiptext #after').css('left', $(item).position().left + centre_offset);
+		$(item).css('z-index', 5)
+		$(item).css('z-index', 5)
+		//// $(item).children().each(function(){$(this).css('z-index', 5);})
+	    $(document).find('.overlay').css('opacity', 0.6);
 		
+		parent.postMessage({label: 'overlay', value:true}, '*');
+	}
+	
+	function resetOverlay(item) {
+		$(item).css('z-index', 0)
+		$(document).find('.overlay').css('opacity', 0);
+		
+		// $(item).children().each(function(){
+			// $(item).css('opacity', 0);
+			// })
+		
+		
+		parent.postMessage({label: 'overlay', value:false}, '*');
 	}
