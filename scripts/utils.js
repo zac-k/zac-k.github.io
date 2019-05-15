3f49e360cf47336e491acc9fc6ac5d36d0ec7f35
Zac <zac.kemp@outlook.com>
2019-05-15 12:36:45 +1000

    function to generate track divs

--- scripts/utils.js
+++ scripts/utils.js
@@ -38,19 +38,43 @@
 		parent.postMessage({label: 'overlay', value:false}, '*');
 	}
 	
-	function generateTrack(track_num) {
+	function generateTrack(track_num, track_name, track_url, track_comment, position) {
 		
-		var div = document.createElement("div");
-		div.class = "tooltip";
-		div.onmouseover = "positionMarker(this); enableOverlay(this)";
-		div.onmouseout = "disableOverlay(this);";
+		var outer = document.createElement("div");
+		outer.setAttribute('class', 'tooltip');
+		outer.setAttribute('onmouseover', "positionMarker(this); enableOverlay(this)");
+		outer.setAttribute('onmouseout', "disableOverlay(this);");
 		
 		var iframe = document.createElement("iframe");
-		iframe.scrolling = "no";
-		iframe.id = "hearthis_at_track_" + track_num.toString();
-		iframe.width = "100%";
-		iframe.height = "130";
-		iframe.src = "https://hearthis.at/embed/" + track_num.toString() + "/transparent/?hcolor=005c61&color=00b0bb&style=2&block_size=5&block_space=2&background=0&waveform=0&cover=1&autoplay=0&css=";
+		iframe.setAttribute('class', 'music');	
+		iframe.setAttribute('scrolling', "no");
+		iframe.setAttribute('id', "hearthis_at_track_" + track_num.toString());
+		// iframe.setAttribute('width', "100%");
+		// iframe.setAttribute('height', "130");		
+		iframe.setAttribute('src', "https://hearthis.at/embed/" + track_num.toString() + "/transparent/?hcolor=005c61&color=00b0bb&style=2&block_size=5&block_space=2&background=0&waveform=0&cover=1&autoplay=0&css=");
+		iframe.setAttribute('frameborder', "0");
+		// iframe.allowtransparency = "true"; // <- not sure if this is needed for anything
+		iframe.setAttribute('allow', "autoplay");		
+		
+		
+		iframe.innerHTML = "<p>Listen to <a href='https://hearthis.at/zac-kemp/" + track_url + "/' target='_blank'>" + track_name + "</a> <span>by</span><a href='https://hearthis.at/zac-kemp/' target='_blank' >Zac Kemp</a> <span>on</span> <a href='https://hearthis.at/' target='_blank'>hearthis.at</a></p>"
+		
+		var tooltip = document.createElement("span");
+		tooltip.setAttribute('class', 'tooltiptext');
+		switch(position){
+			case "c":
+				tooltip.className += ' pos_c';
+				break;
+			case "r":
+				tooltip.className += ' pos_r';
+				break;
+		}
+		tooltip.innerHTML = "<div id='before'> </div><div id='after'></div>" + track_comment;
+		
+		outer.appendChild(iframe);
+		outer.appendChild(tooltip);
+		console.log(outer);
+		$('#main').append(outer);
 		
 		
 		
